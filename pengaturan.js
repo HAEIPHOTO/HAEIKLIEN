e91f04815a86822b07926e9f0b3aa46959fdaa16
HAEIPHOTO <justerza95@gmail.com>
2026-09-17 21:16:09 +0800

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "HAEIPHOTO",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
+    googleApiKey: "AIzaSyBQwHaxqgEgI9cVvzg295e80zKXUPib-T8",
     whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
